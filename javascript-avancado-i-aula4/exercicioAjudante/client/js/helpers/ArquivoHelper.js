770bc182ced50946c02b7dd10ec7afd3234ca511
fossalussa <34274776+fossalussa@users.noreply.github.com>
2019-03-18 09:18:27 -0300

    Update ArquivoHelper.js

--- javascript-avancado-i-aula4/exercicioAjudante/client/js/helpers/ArquivoHelper.js
+++ javascript-avancado-i-aula4/exercicioAjudante/client/js/helpers/ArquivoHelper.js
@@ -1,5 +1,9 @@
 class ArquivoHelper {
 
+    constructor () {
+        throw new Error('Não é permitido instanciar essa Classe!');
+    }
+
     static cria(informacao) {
         return new Arquivo(...informacao.toUpperCase().split('/'));
     };
