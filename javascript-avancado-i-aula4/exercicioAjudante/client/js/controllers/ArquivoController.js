cbb0784e664a63f819eee4880b6e08dc3ffef6cd
fossalussa <34274776+fossalussa@users.noreply.github.com>
2019-03-18 08:59:32 -0300

    Update ArquivoController.js

--- javascript-avancado-i-aula4/exercicioAjudante/client/js/controllers/ArquivoController.js
+++ javascript-avancado-i-aula4/exercicioAjudante/client/js/controllers/ArquivoController.js
@@ -6,6 +6,12 @@ class ArquivoController {
     }
 
     envia() {
+
+        //let dados = this._inputDados.value.split('/').map(item => item.toUpperCase());
+        // let dados = this._inputDados.value.toUpperCase().split('/');
+        let arquivo = ArquivoHelper.cria(this._inputDados.value);
+        console.log(`Dados do arquivo: "${arquivo.nome}, ${arquivo.tamanho}, ${arquivo.tipo}"!`);
+        
         //cria um Arquivo com as suas propriedades;
         this._limpaFormulario();
         // exibe mensagem no console com os dados do arquivo.
